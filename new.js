460b72488ddede80c274339a88c5856ab9a855bd
Ethan <eeckard08@gmail.com>
2022-01-07 13:15:15 -0500

    clean version

--- new.js
+++ new.js
@@ -1,111 +1,5 @@
 console.log("hello world")
-/*
- function playerOneprompt() {
-    let playerOne = prompt("please input your selection", ""); // this is to prompt the user
-    console.log("Player one has " + playerOne);
-    return playerOne;
-}
 
-//let playerOne = prompt("please input your selection", ""); // this is to prompt the user
-//console.log("Player one has " + playerOne); // this adds a console log to indicate player one's selection
-/*
-let playerOne = playerOneprompt();
-var result = [4];
-*/
-
-/* this was a method to determine an automatic choice for player one
-function playerOne(){
-    let x = Math.floor(Math.random() * 100 + 1);
-    if(x < 33) {
-        console.log("player One has rock");
-        return "rock";
-    }
-    else if(x >= 33 && x < 66) {
-        console.log("player One has paper");
-        return "paper";
-    }
-    else {
-        console.log("player One has scissors");
-        return "scissors";
-    }
-}
-*/
-/*
-function playerTwo(){
-    let x = Math.floor(Math.random() * 100 + 1);
-    if(x < 33) {
-        console.log("player Two has rock");
-        return "rock";
-    }
-    else if(x >= 33 && x < 66) {
-        console.log("player Two has paper");
-        return "paper";
-    }
-    else {
-        console.log("player Two has scissors");
-        return "scissors";
-    }
-}
-//these are the possible outcomes for each case
-const case1 = "Player one wins";
-const case2 = "Player two wins";
-const case3 = "Draw";
-
-// this is the logic for each outcome
-function gamePlay(x, y) {
-    if (x === "rock" && y === "rock") {
-        return "Draw";
-    }
-    else if (x === "rock" && y === "scissors") {
-        return "Player one wins";
-    }
-    else if (x === "rock" && y === "paper") {
-        return "Player two wins";
-    }
-    else if (x ==="scissors" && y === "scissors") {
-        return "Draw";
-    }
-    else if (x ==="scissors" && y === "rock") {
-        return "Player two wins";
-    }
-    else if (x ==="scissors" && y === "paper") {
-        return "Player one wins";
-    }
-    else if (x ==="paper" && y === "paper") {
-        return "Draw";
-    }
-    else if (x === "paper" && y === "scissors") {
-        return "Player two wins";
-    }
-    else {
-        return "Player one wins";
-    }
-}
-
-function gameCounter() {
-    let i = 0;
-    while (i < 5) {
-        let playerOne = playerOneprompt();
-        alert(gamePlay(playerOne.toLowerCase,playerTwo()));
-        i++;
-    }
-}
-
-//gameCounter();
-/*alert(gamePlay(playerOne.toLowerCase,playerTwo()));
-result[0] = gamePlay(playerOne.toLowerCase,playerTwo);
-*/
-//console.log(result[0]);
-
-
-
-/*function computerPlay(a,b){
-    if (a === b) {
-        return "Draw";
-    }
-    else 
-};
-*/
 let initiate = prompt("would you like to play? y/n", "");
 
 const case1 ="Player one wins";
@@ -119,8 +13,9 @@ var game = [];
 // these are just test console logs to check what each player has chosen quickly
 console.log("player one has chosen", player1);
 console.log("Player two has chosen", player2);
+initiateGame();
+
 //console.log(initiate);
-addEventListener("click", initiateGame());
 //this will call all the functions together
 
 
@@ -149,7 +44,7 @@ function playerOneprompt() {
 
 // this is the prompt to start the game //
 function initiateGame(){
-    if(initiate === "y") {
+        if(initiate === "y") {
          alert("Okay, let's play! ");
          alert("Best 3 out of 5 wins the tournament");
          gameCounter();
@@ -162,7 +57,7 @@ function initiateGame(){
         else {
             alert("I'm sorry I'm not sure what you meant")
         }
-    }
+}
 //This is the game logic
 function gameLogic() {
     if(player1 === "rock" && player2 === "rock" || player1 === "paper" && player2 === "paper" || player1 ==="scissors" && player2 ==="scissors"){
@@ -209,9 +104,5 @@ function gameWinner (x){
         alert("Player One wins the tournament");
     }
 
-        }
-
-//gameCounter();
-//console.log(game);
-//gameWinner(gameAdder(game));
-
+}
+// i'm gonna try to make a function that returns factorials
